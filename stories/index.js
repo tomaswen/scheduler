daa06acf85e1463e6eee5bd9dafca90ef930a3fa
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-13 18:59:15 -0400

    update click story for interviewer list item

--- stories/index.js
+++ stories/index.js
@@ -96,10 +96,9 @@ storiesOf("Button", module)
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={action("setInterviewer")}
+        setInterviewer={event => action("setInterviewer")(interviewer.id)}
       />
     ));
-
 
     const interviewers = [
       { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
