c64d9280e3c2cbbc61aeb217dd6baf8872581743
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-13 18:57:18 -0400

    display a list of interviwers

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {props.interviewers.map((interviewer) => {
+          return (
+            <InterviewerListItem
+               key = {interviewer.id}
+              name={interviewer.name}
+              avatar={interviewer.avatar}
+              selected={interviewer.id === props.interviewer}
+              setInterviewer={() => props.setInterviewer(interviewer.id)}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
