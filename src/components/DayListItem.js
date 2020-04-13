46420742c0eb391659fb45a79283779798d8c3a7
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-13 16:25:08 -0400

    Render list of a individual day and styling

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "components/DayListItem.scss";
+const classNames = require("classnames");
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+  
+  const formatSpots = (spots) => {
+    let spotMessage = "";
+    spots
+      ? spots > 1
+        ? (spotMessage = spots + " spots remaining")
+        : (spotMessage = "1 spot remaining")
+      : (spotMessage = "no spots remaining");
+    return spotMessage;
+  };
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
