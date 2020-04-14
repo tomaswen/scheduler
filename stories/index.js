ced822e7b8255a4df1d5bf890f47ca93885fd019
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-13 20:46:06 -0400

    Add status component and story for appointment

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -158,4 +159,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", ()=> <Status message="Deleting" />)
