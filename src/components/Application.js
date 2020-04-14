b39c7fb8dff150b5b2d3ced47f18c1a7db5b581b
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-14 17:02:55 -0400

    added axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
+const axios = require('axios');
 
 const appointments = [
   {
