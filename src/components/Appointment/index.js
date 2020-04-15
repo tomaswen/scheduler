bce431a3792e6245185813ccb2fea0da2df7b2b4
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-15 17:42:41 -0400

    add transition from empty to create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,35 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  function onAdd() {
+    transition("CREATE");
+  }
+  function onCancel() {
+    back();
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={onCancel} />}
     </article>
   );
 }
