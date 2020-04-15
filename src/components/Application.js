9bb8c66c6440f55d727406387ba81d9849ef2623
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-15 14:56:21 -0400

    remove console log

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,22 +16,23 @@ export default function Application(props) {
 
   const setDay = (day) => setState({ ...state, day });
   useEffect(() => {
-    Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")]).then(
-      (all) => {
-        console.log(all)
-        setState((prev) => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data,
-        }));
-      }
-    );
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
-  
+
   const appointmentsForDayArray = getAppointmentsForDay(state, state.day);
   const schedule = appointmentsForDayArray.map((appointment) => {
-    const interview = getInterview(state, appointment.interview)
+    const interview = getInterview(state, appointment.interview);
     return (
       <Appointment
         key={appointment.id}
