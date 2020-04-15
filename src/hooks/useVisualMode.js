ae6636aa2b28b81a06fc225358da72bf6a19d926
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-04-15 17:14:49 -0400

    Finish custom hook and pass test

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      history.push(newMode);
+    } else {
+      history[history.length - 1] = newMode;
+    }
+    setMode(newMode);
+  }
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else {
+      setMode(history[0]);
+    }
+  }
+
+  return { mode, transition, back };
+}
